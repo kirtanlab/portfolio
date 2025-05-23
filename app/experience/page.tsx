21022b344e1a5b4f87a3a860221e7dd928fd0ed6
kirtanlab <kirtanprajapati0804@gmail.com>
2025-05-23 10:13:04 -0400

    Experience updated

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,25 +5,31 @@ const Experience = () => {
         companyLink: "https://www.nahar.om/",
         position: "Software Developer",
         period: "May 2024 - October 2024",
-        description: [
-          "My role encompassed full-stack development with expertise in financial systems and trading mechanisms.",
-          "Conducted detailed requirement gathering using agile methodology, implemented React with Redux state management, developed RESTful APIs with Django Rest Framework, designed PostgreSQL database architecture and maintaining CI/CD Pipeline with Docker containers.",
-          "Additional responsibilities included creating minimalist UI integrations and maintaining weekly client communication to ensure alignment with business objectives."
-        ],
-        technologies: "React, Redux, Django, REST API, PostgreSQL, Docker, Boost Money, Signzy"
+        // description: [
+        //   "My role encompassed full-stack development with expertise in financial systems and trading mechanisms.",
+        //   "Conducted detailed requirement gathering using agile methodology, implemented React with Redux state management, developed RESTful APIs with Django Rest Framework, designed PostgreSQL database architecture and maintaining CI/CD Pipeline with Docker containers.",
+        //   "Additional responsibilities included creating minimalist UI integrations and maintaining weekly client communication to ensure alignment with business objectives."
+        // ],
+        // technologies: "React, Redux, Django, REST API, PostgreSQL, Docker, Boost Money, Signzy"
       },
       {
         company: "Tech Elecon | Anand, India",
         companyLink: "https://portfolio-uuoj.onrender.com/www.techelecon.com",
         position: "Software Engineering Intern",
         period: "January 2024 - April 2024",
-        description: [
-          "I led critical projects with unwavering commitment, navigating tight deadlines and collaborating seamlessly with colleagues.",
-          "My role extended beyond coding, as I also provided guidance to 50+ peers on parallel projects, ensuring a cohesive and successful internship experience.",
-          "Technically, I implemented full-stack solutions using Spring Boot backend and React frontend with Redux state management, all connected to PostgreSQL databases.",
-          "I demonstrated proficiency in translating SRS documentation into working features and integrated adaptable UI themes, including minimalist and dark mode variants, enhancing user experience across multiple platforms."
-        ],
-        technologies: "Spring Boot, REST API, PostgreSQL, JWT, React, Reduc"
+        // description: [
+        //   "I led critical projects with unwavering commitment, navigating tight deadlines and collaborating seamlessly with colleagues.",
+        //   "My role extended beyond coding, as I also provided guidance to 50+ peers on parallel projects, ensuring a cohesive and successful internship experience.",
+        //   "Technically, I implemented full-stack solutions using Spring Boot backend and React frontend with Redux state management, all connected to PostgreSQL databases.",
+        //   "I demonstrated proficiency in translating SRS documentation into working features and integrated adaptable UI themes, including minimalist and dark mode variants, enhancing user experience across multiple platforms."
+        // ],
+        // technologies: "Spring Boot, REST API, PostgreSQL, JWT, React, Reduc"
+      }
+      {
+        company: "Windsor Analytics | Windsor, ON, Canada",
+        companyLink: "https://windsoranalytics.ca/",
+        position: "Windsor Analytics",
+        period: "January 2025 - April 2025",
       }
     ];
   
